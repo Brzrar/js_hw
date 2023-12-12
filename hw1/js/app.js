9479b1032a351194eccdee8f200da45098847593
Basirahmanov444 <113968574+Brzrar@users.noreply.github.com>
2023-12-12 12:00:04 +0600

    Delete hw1 directory

--- hw1/js/app.js
+++
@@ -1,7 +0,0 @@
-let num = '21709200401019'
-let firstNum = Number(num[0]);
-if ((firstNum === 0 || firstNum === 1 || firstNum === 2) && num.length == 14 ){
-    console.log('inn true')
-}else {
-    console.log('inn false')
-}
