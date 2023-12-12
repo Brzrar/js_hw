f32371c691b348821bc6fb4178d47460991a2824
Basirahmanov444 <113968574+Brzrar@users.noreply.github.com>
2023-12-12 12:00:45 +0600

    Add files via upload

---
+++ hw1_Raimjan_33-01/app.js
@@ -0,0 +1,49 @@
+// 1 dz
+
+// Сфера продажи
+
+// 1. Числовой тип данных
+var numberSold = 50; // Количество проданных товаров
+
+// 2. Строковый тип данных
+var name = "Iphone 15"; // Название товара
+
+// 3. Логический тип данных
+var inStock = true; // Показывает, наличие товара (true/false)
+
+// 4. Тип данных "undefined"
+var price ; // Цена товара (значение еще не присвоено)
+
+// 5. Тип данных "null"
+var characteristic = null; // Описание товара, еще не заполнено
+
+// 6. Объектный тип данных
+var productInformation = {
+    brand: "Iphone",
+    color: "black",
+    Tipe: "smartphone",
+    model: "15"
+}; // Информацию о продукте в виде объекта
+
+// 2 dz
+
+var filialInformation = {
+    address: 'ул. Логвиненко, 21/2 Бишкек',
+    main: 'no',
+    numberofEmployees: 10,
+    schedule: {
+        c: '9:00 AM',
+        do: '17:00 PM'
+    }
+}
+
+// 3 dz
+
+var studentsInformation = {
+    name: 'Raimjan',
+    age: 19,
+    monthTraining: 2,
+    direction: 'Front end'
+}
+
+
